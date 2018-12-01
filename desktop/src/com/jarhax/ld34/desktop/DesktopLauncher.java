842ca3af2ce37dfbef4e3cbebf4ab53b8ee7c9ab
Tyler Hancock <darklime@live.ca>
2018-12-01 01:16:43 -0700

    Default LibGDX code.

---
+++ desktop/src/com/jarhax/ld34/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jarhax.ld34.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jarhax.ld34.LD34;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD34(), config);
+	}
+}
