07e867052e36d7c04b5dd5376cf1acdfea44cc59
Jared <jaredlll08@gmail.com>
2018-12-02 07:35:56 +0200

    Moved to model cache and pregenned map

--- core/src/com/jarhax/ld34/LD34.java
+++ core/src/com/jarhax/ld34/LD34.java
@@ -16,21 +16,21 @@ public class LD34 extends ApplicationAdapter {
     public Environment environment;
     public PerspectiveCamera cam;
     public ModelBatch modelBatch;
-    public List<ModelInstance> instances = new ArrayList<ModelInstance>();
     public FirstPersonCameraController camController;
     private static final OpenSimplexNoise NOISE = new OpenSimplexNoise();
     
-    Map<Pos, GameObject> blocks = new HashMap<Pos, GameObject>();
+    List<GameObject> blocks = new ArrayList<GameObject>();
     ModelBuilder modelBuilder;
     Model model;
     
     Random rand = new Random();
     
+    
     @Override
     public void create() {
         environment = new Environment();
-        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 1, 1, 1, 1f));
+        environment.add(new DirectionalLight().set(1f, 1f, 1f, -1f, -0.8f, -0.2f));
         modelBatch = new ModelBatch();
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.position.set(10f, 10f, 10f);
@@ -45,31 +45,27 @@ public class LD34 extends ApplicationAdapter {
         Gdx.input.setInputProcessor(camController);
         
         modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(1f, 1f, 1f, new Material(ColorAttribute.createDiffuse(new Color((int) (0x200080)))), VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-    }
-    
-    Vector3 dimensions = new Vector3(1, 1, 1);
-    float lastX = 0;
-    float lastZ = 0;
-    float totalTime = 0;
-    
-    int lastTime = 0;
-    
-    public static class GameObject extends ModelInstance {
+        Material material = new Material(ColorAttribute.createDiffuse(new Color((int) (0x200080))));
+        model = modelBuilder.createBox(1f, 1f, 1f, material, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         
-        public final Vector3 center = new Vector3();
-        public final Vector3 dimensions = new Vector3();
-        public final float radius;
         
-        private final static BoundingBox bounds = new BoundingBox();
+        int dist = 512;
         
-        public GameObject(Model model) {
-            super(model);
-            calculateBoundingBox(bounds);
-            bounds.getCenter(center);
-            bounds.getDimensions(dimensions);
-            radius = dimensions.len() / 2f;
+        for(int x = -dist; x < dist; x++) {
+            for(int z = -dist; z < dist; z++) {
+                float xx = Math.round(cam.position.x) + x;
+                float zz = Math.round(cam.position.z) + z;
+                float y = (float) Math.round(NOISE.eval(xx / 128f, zz / 128f) * 64);
+                GameObject e = new GameObject(model, new Pos(xx, y, zz));
+                e.transform.setToTranslation(xx, y, zz);
+                blocks.add(e);
+            }
         }
+        
+        modelCache.begin();
+        modelCache.add(blocks);
+        modelCache.end();
+        
     }
     
     
@@ -81,46 +77,20 @@ public class LD34 extends ApplicationAdapter {
         return cam.frustum.sphereInFrustum(position, instance.radius);
     }
     
+    ModelCache modelCache = new ModelCache();
+    
     @Override
     public void render() {
-        totalTime += Gdx.graphics.getDeltaTime();
         camController.update();
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         Gdx.graphics.setTitle("FPS: " + Gdx.graphics.getFramesPerSecond());
+        
         modelBatch.begin(cam);
-        for(GameObject instance : blocks.values()) {
-            System.out.println(instance.model.meshes.size);
-            if(isVisible(instance)) {
-                
-                modelBatch.render(instance, environment);
-            }
-        }
+        
+        modelBatch.render(modelCache, environment);
+        
         modelBatch.end();
-        
-        
-        int i = 5;
-        if(Math.round(totalTime) % 0.5f == 0/*Math.abs(lastX - cam.position.x) > i || Math.abs(lastZ - cam.position.z) > i*/) {
-            int dist = 50;
-            for(int x = -dist; x < dist; x++) {
-                for(int z = -dist; z < dist; z++) {
-                    float xx = Math.round(cam.position.x) + x;
-                    float zz = Math.round(cam.position.z) + z;
-                    
-                    float y = (float) Math.round(NOISE.eval(xx / 128f, zz / 128f) * 64);
-                    GameObject e = new GameObject(model);
-                    e.transform.setTranslation(xx, y, zz);
-                    
-                    for(Material material : e.materials) {
-                        material.set(ColorAttribute.createDiffuse(new Color((int) (0x7BCCB5 * y))));
-                    }
-                    Pos key = new Pos(xx, y, zz);
-                    blocks.put(key, e);
-                }
-            }
-            lastX = cam.position.x;
-            lastZ = cam.position.z;
-        }
         
         
     }
@@ -128,5 +98,46 @@ public class LD34 extends ApplicationAdapter {
     @Override
     public void dispose() {
         modelBatch.dispose();
+    }
+    
+    
+    public static class GameObject extends ModelInstance {
+        
+        public final Vector3 center = new Vector3();
+        public final Vector3 dimensions = new Vector3();
+        public final float radius;
+        
+        private final static BoundingBox bounds = new BoundingBox();
+        private Pos pos;
+        
+        public GameObject(Model model, Pos pos) {
+            super(model);
+            calculateBoundingBox(bounds);
+            bounds.getCenter(center);
+            bounds.getDimensions(dimensions);
+            radius = dimensions.len() / 2f;
+            this.pos = pos;
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if(this == o)
+                return true;
+            if(o == null || getClass() != o.getClass())
+                return false;
+            
+            GameObject pos = (GameObject) o;
+            
+            if(Float.compare(pos.pos.getX(), this.pos.getX()) != 0)
+                return false;
+            return Float.compare(pos.pos.getZ(), this.pos.getZ()) == 0;
+        }
+        
+        @Override
+        public int hashCode() {
+            int result = (pos.getX() != +0.0f ? Float.floatToIntBits(pos.getX()) : 0);
+            result = 31 * result + (pos.getZ() != +0.0f ? Float.floatToIntBits(pos.getZ()) : 0);
+            return result;
+        }
     }
 }
